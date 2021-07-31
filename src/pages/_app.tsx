5e56cd42224e81f09802bda21c49e0071e81fd0d
abhinavpurohit47 <purohitabhinav01@gmail.com>
2021-07-31 20:16:22 +0530

    🎉 UI Design Begin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,15 @@ import "tailwindcss/tailwind.css";
 import "../../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="h-screen font-serif text-white bg-gray-800 grid place-items-center">
+  
+    <div>
+    <Component {...pageProps} />
+    </div>
+    </div> 
+  )
 }
+
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import Delete from "@components/Delete";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gray-800">
-      <Delete />
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="my-4 text-3xl font-medium">
+        Share your File with The Disseminator!!
+      </h1>
+      <div className="flex flex-col items-center justify-center bg-gray-700 shadow-xl w-96 rounded-xl">
+        dropzone
+        {/* render file */}
+        {/* upload button */}
+      </div>
     </div>
   );
 }
